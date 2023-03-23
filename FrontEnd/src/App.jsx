f92040bd890422625e527bd318d3e9af1d424663
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2023-03-23 01:05:58 -0300

    Nuevo CSS + SimpleMDE-React

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import './App.scss'
-import { UploadIcon, DownloadIcon } from '@primer/octicons-react'
-import axios from 'axios'
-import fileDownload from 'js-file-download'
-
-import Logo from '/Logo-U.png'
+import { UploadIcon, DownloadIcon } from '@primer/octicons-react';
+import SimpleMdeReact from 'react-simplemde-editor';
+import fileDownload from 'js-file-download';
+import { useState, useCallback } from 'react';
+import axios from 'axios';
+import "easymde/dist/easymde.min.css";
+import './App.scss';
 
 function App() {
-    const [archiveType, setArchiveType] = useState("")
-    const [selectedFile, setSelectedFile] = useState(null)
+    const [archiveType, setArchiveType] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
     const [downloadUrl, setDownloadUrl] = useState("");
-    const [loading, setLoading] = useState(false)
-    const [download, setDownload] = useState(false)
-    const [errorCode, setErrorCode] = useState("")
+    const [loading, setLoading] = useState(false);
+    const [download, setDownload] = useState(false);
+    const [errorCode, setErrorCode] = useState("");
 
     const handleFileInputChange = (e) => {
         setSelectedFile(e.target.files[0])
@@ -45,12 +45,12 @@ function App() {
                     },
                     responseType: "blob",
                 });
-                downloadFile =  new Blob([response.data]);
+                downloadFile = new Blob([response.data]);
                 return downloadFile
             } catch (error) {
                 const statusCode = error.response?.status ?? 500; // Usamos el operador de encadenamiento opcional y de fusión nula para manejar el caso en que error.response sea undefined
                 setErrorCode(statusCode)
-                
+
             } finally {
                 setLoading(false)
                 setDownloadUrl(downloadFile);
@@ -71,10 +71,10 @@ function App() {
         let options = ["pdf", "csv", "xls", "xlsx", "doc", "docx"];
         let extention = extractExtention(file);
         options.map((option, i) => {
-            if (option === extention){
+            if (option === extention) {
                 options.splice(i, 1)
             }
-            if ((extention === "pdf" && option === "html") /*|| (extention === "pdf" && option === "doc")*/){
+            if ((extention === "pdf" && option === "html") /*|| (extention === "pdf" && option === "doc")*/) {
                 options.splice(i, 1)
             }
         })
@@ -93,7 +93,7 @@ function App() {
         return fileName;
     }
 
-    function loadingFunc(boolean){
+    function loadingFunc(boolean) {
         if (boolean) {
             return (
                 <span className="py-2 px-4 flex justify-center items-center  bg-blue-600 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md rounded-lg w-36 mx-auto">
@@ -112,7 +112,7 @@ function App() {
     function downloadButton(boolean) {
         if (boolean) {
             return (
-                <button onClick={() => {fileDownload(downloadUrl, `${getFileName(selectedFile.name)}.zip`)}}> <DownloadIcon size={24}/> Descargar</button>
+                <button onClick={() => { fileDownload(downloadUrl, `${getFileName(selectedFile.name)}.zip`) }}> <DownloadIcon size={24} /> Descargar</button>
             )
         } else {
             return (<span></span>)
@@ -129,34 +129,61 @@ function App() {
         }
     }
 
-    return (
-        <div className='App flex flex-col'>
-            <div className='flex flex-row justify-around items-center gap-y-14'>
-                <img id='logo-img' className='py-5' src={Logo} alt="Logo" />
-                <div id="titulo">
-                    <div>
-                        <h1 id='texto-titulo' className='text-xl font-bold text-white text-center py-6'>
-                        DESARROLLO DE UN COMPONENTE PARA LA ELABORACIÓN DE REPORTES WEB DEL LADO DEL CLIENTE UTILIZANDO LOS FORMATOS HTML, PDF Y EXCEL.
-                        </h1>
+    const Header = () => {
+        return (
+            <div className="header">
+                <div className='logo' />
+                <div className='title'>DESARROLLO DE UN COMPONENTE PARA LA ELABORACIÓN DE REPORTES WEB DEL LADO DEL CLIENTE UTILIZANDO LOS FORMATOS HTML, PDF Y EXCEL</div>
+            </div>
+        )
+    }
+
+    const SubirArchivo = () => {
+        return (
+            <form id='formButton' onSubmit={handleFileSubmit} className='centered'>
+                <label id='labelButton' htmlFor="fileInput" className='uploadButton'>
+                    <span><UploadIcon size={24} id="uploadIcon" /> Subir archivo</span>
+                </label>
+                <input type="file" id='fileInput' accept='.pdf, .csv, .xlsx, .xls, .doc, .docx' onChange={handleFileInputChange} className="hidden" />
+            </form>
+        )
+    }
+
+    const MDE = () => {
+        const [texto, setTexto] = useState("");
+        const onChange = useCallback((value) => {
+            setTexto(value);
+        }, []);
+
+        return <SimpleMdeReact value={texto} onChange={onChange} />;
+    };
+
+    const Modal = () => {
+        return (
+            <>
+                <input class="modal-state" id="modal-1" type="checkbox" />
+                <div class="modal">
+                    <label class="modal__bg" for="modal-1"></label>
+                    <div class="modal__inner">
+                        <label class="modal__close" for="modal-1"></label>
+                        <MDE />
                     </div>
                 </div>
-            </div>
-            <div>
-                <form id='formButton' onSubmit={handleFileSubmit} className='py-2'>
-                    <label id='labelButton' htmlFor="fileInput" className="py-2 px-4 flex w-40 justify-center items-center bg-red-600 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md  rounded-lg mx-auto">
-                        <span className='mr-2 transition'>
-                            <UploadIcon size={24} id="uploadIcon"/>
-                        </span>
-                        Subir archivo
-                    </label>
-                    <input type="file" id='fileInput' accept='.pdf, .csv, .xlsx, .xls, .doc, .docx' onChange={handleFileInputChange} className="hidden"/>
-                </form>
+            </>
+        )
+    }
 
-                {selectedFile && !checkFileExtention(selectedFile) && (
-                    <h2 className='py-2'>El archivo de extención {extractExtention(selectedFile)} no es valido</h2>
-                )}
+    return (
+        <>
+            <Header />
+            <div className='contenido'>
+                <SubirArchivo />
+                <Modal />
 
-                {selectedFile && checkFileExtention(selectedFile) && (
+                {selectedFile && !checkFileExtention(selectedFile) && <h2 className='py-2'>El archivo de extención {extractExtention(selectedFile)} no es valido</h2>}
+
+                {
+                    selectedFile && checkFileExtention(selectedFile) &&
                     <div>
                         <h2 className='py-2'>Archivo seleccionado: {selectedFile.name}</h2>
                         <select id='archiveType' className="block px-3 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm w-52 focus:outline-none focus:ring-primary-500 focus:border-primary-500 mx-auto my-2" name="animals" value={archiveType} onChange={(e) => setArchiveType(e.target.value)}>
@@ -166,22 +193,19 @@ function App() {
                             {selectOptions(selectedFile)}
                         </select>
                         <button type='submit' onClick={handleClick} className='my-2 bg-[#1a1a1a]'>Convertir</button>
-                        <div>
+                        <>
                             {loadingFunc(loading)}
                             {errorMessages(errorCode)}
-                        </div>
+                        </>
                     </div>
-                )}
-
-                {!selectedFile && (
-                    <h2 className='py-2'>No hay un archivo seleccionado</h2>
-                )}
-
-                {downloadUrl &&
-                    downloadButton(download)
                 }
+
+                {!selectedFile && <h2 className='centered selectedFile'>No hay un archivo seleccionado</h2>}
+                {downloadUrl && downloadButton(download)}
+
+                <label class="writeButton" for="modal-1">Escribir un archivo</label>
             </div>
-        </div>
+        </>
     )
 }
 
